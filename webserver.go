ed298350a197ace2fa197b43c749c27c80142b4a
Andres <barathum196@gmail.com>
2018-07-10 17:52:56 -0500

    added a few functions to create channels and a slice of a channel

--- webserver.go
+++ webserver.go
@@ -11,11 +11,25 @@ import (
 )
 
 var upgrader = websocket.Upgrader{}
-var connections []*websocket.Conn
+var connections []*websocket.Conn //Connections stablished
+
+//Creating a struct for the channels
+type chatChannel struct {
+	channelName string `json:"channelName"` //A name asigned to the channel(Default is always present)
+	path        string `json:"path"`        //the path that leads to the channel(Need a handler for this)
+}
+
+type chatChannels struct {
+	chatChannels []chatChannel `json:"chatChannels"`
+}
 
 //Today's objective: Make different channels that people can use to comunicate. i have no clue how to do this
 func main() {
+	var c chatChannels
 	router := mux.NewRouter()
+	//Creating Default channel for all the newcomers
+	nChannel := createChannel("Default")
+	c.chatChannels = append(c.chatChannels, nChannel)
 	router.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		var conn, _ = upgrader.Upgrade(w, r, w.Header())
 		connections = append(connections, conn)
@@ -54,4 +68,12 @@ func main() {
 	}
 
 	panic(server.ListenAndServe())
+}
+
+func createChannel(channName string) chatChannel {
+	chPath := "/" + channName
+	return chatChannel{
+		channelName: channName,
+		path:        chPath,
+	}
 }
